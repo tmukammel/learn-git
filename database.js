1b67635d84ddf4b5422a45a28441d8c30eaacfd6
ulkabd <ulka@ulkabd.com>
2019-02-27 13:27:39 +0600

    FEATURE: Connect to database

---
+++ database.js
@@ -0,0 +1,27 @@
+var mysql = require('mysql');
+
+const database = 'cholon_dev';
+
+var con = mysql.createConnection({
+  host: "127.0.0.1",
+  user: "root",
+  password: "",
+  database
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
+exports.connection = con;
+
+exports.endConnection = () => {
+    con.end((error) => {
+        if (error) console.log(`Could not disconnect database due to ${error}`);
+        else {
+            con = null;
+            console.log(`${database} disconnected successfully!`);
+        }
+    });
+}
